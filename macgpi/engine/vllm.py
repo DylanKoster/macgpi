d1d24af9abb978e6ca8a844349833ba448cede35
DylanKoster <dylankoster40@gmail.com>
2026-05-06 22:36:14 +0200

    Use requests instead of urllib

--- macgpi/engine/vllm.py
+++ macgpi/engine/vllm.py
@@ -1,23 +1,24 @@
 import logging
 
-import urllib
+import requests
 
 logger = logging.getLogger(__name__)
 
 def vllm_health(host: str, port: int) -> bool:
     '''
-    Test the health of the vLLM server by sending a request to the models endpoint. Returns True if the server is
+    Test the health of the vLLM server by sending a request to the health endpoint. Returns True if the server is
     healthy and reachable, False otherwise.
     '''
-    url: str = f"http://{host}:{port}/v1/models",
+    url: str = f"http://{host}:{port}/health"
         
-    logger.debug("Testing vLLM endpoint...")
+    logger.debug(f"Testing vLLM endpoint ({url})...")
     try:
-        with urllib.request.urlopen(url, timeout=2) as response:
-            if 200 <= response.status < 300:
-                return True
-        
+        resp = requests.get(url, timeout=2)
+        logger.debug(f"vLL response: {resp}")
+        if 200 <= resp.status_code < 300:
+            return True
     except Exception:
+        logger.debug("Exception while testing vLLM endpoint", exc_info=True)
         pass
 
     return False

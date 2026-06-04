5e106f504c4b4f7aef0d471f32d395abd81851ea
DylanKoster <dylankoster40@gmail.com>
2026-06-04 23:13:50 +0200

    Test vLLM connection each phase

--- macgpi/engine/main.py
+++ macgpi/engine/main.py
@@ -56,12 +56,14 @@ class MACGPi:
 
         # Instantiating managers
         try:
-            self.templateManager: TemplateManager = TemplateManager(prompt_dir=self.prompt_dir)
+            self.templateManager: TemplateManager = TemplateManager(
+                prompt_dir=self.prompt_dir)
             self.agentManager: AgentManager = AgentManager(self.model_name, model_host=model_host,
                                                            model_port=model_port, model_config_file=model_config_file,
                                                            agent_config_file=agent_config_file)
         except Exception:
-            logger.error(f"An error occurred while initializing MACGPi:\nError {traceback.format_exc()}")
+            logger.error(
+                f"An error occurred while initializing MACGPi:\nError {traceback.format_exc()}")
 
     def run(self) -> bool:
         '''
@@ -75,7 +77,8 @@ class MACGPi:
             macgpi_config = self.load_phases_config()
             phases_config = macgpi_config["phases"]
             if not self.validate_macgpi_config(macgpi_config):
-                logger.error("Phase configuration is not valid, cannot run MACGPi.")
+                logger.error(
+                    "Phase configuration is not valid, cannot run MACGPi.")
                 return False
 
             # -------------------------------------
@@ -103,13 +106,18 @@ class MACGPi:
                 phase = macgpi_config["entry"]
 
             while not is_finished_phase(phase):
+                if not self.test_vllm_connection():
+                    logger.error("Lost connection to vLLM server, aborting execution.")
+                    return False
+                
                 phase_config: dict = phases_config[phase]
 
                 # Test whether max_visits has been exceeded for this phase, and if so, move to the
                 # max_visits_exceeded_next
                 max_visits: int = phase_config.get("max_visits", 1)
                 if (self.phase_visits[phase] >= max_visits):
-                    next_phase = phase_config.get("max_visits_exceeded_next", "finish")
+                    next_phase = phase_config.get(
+                        "max_visits_exceeded_next", "finish")
                     logger.info(f"Max visits exceeded for phase {phase}, moving to max_visits_exceeded_next phase "
                                 + f"{next_phase}...")
                     phase = next_phase
@@ -121,14 +129,17 @@ class MACGPi:
 
                 output_file = None
                 if "output_file" in phase_config.keys():
-                    output_file = os.path.join(self.output_dir, phase_config["output_file"])
+                    output_file = os.path.join(
+                        self.output_dir, phase_config["output_file"])
 
-                inputs: dict = read_phase_inputs(phase_config["inputs"], self.output_dir)
+                inputs: dict = read_phase_inputs(
+                    phase_config["inputs"], self.output_dir)
 
                 prompts: list[str] = get_phase_prompts(os.path.join(
                     self.templateManager.prompt_dir, phase_config["path"]))
                 for prompt_file in prompts:
-                    logger.info(f"Running prompt {prompt_file} for phase {phase}...")
+                    logger.info(
+                        f"Running prompt {prompt_file} for phase {phase}...")
                     template_output: str = self.templateManager.render(phase_config["path"], template_file=prompt_file,
                                                                        output_file=output_file, **inputs)
                     self.agentManager.run(template_output)
@@ -140,6 +151,8 @@ class MACGPi:
                                                     phase_config["path"], "schema.json")
 
                     if not self.validate_output(output_file, schema_path, phase):
+                        logger.warning(
+                            f"Output for phase {phase} did not validate against the schema. Re-running phase...")
                         self.phase_visits[phase] -= 1
                         continue
 
@@ -147,10 +160,12 @@ class MACGPi:
                 phase = get_next_phase(
                     phase_config, output_dir=self.output_dir)
 
-            logger.info(f"MACGPi execution finished, result copied to {self.output_dir}")
+            logger.info(
+                f"MACGPi execution finished, result copied to {self.output_dir}")
             return True
         except Exception:
-            logger.error(f"An error occured while executing MACGPi:\nError {traceback.format_exc()}")
+            logger.error(
+                f"An error occured while executing MACGPi:\nError {traceback.format_exc()}")
             return False
 
     def get_phase_visits(self) -> dict:
@@ -164,7 +179,8 @@ class MACGPi:
         Tests the connection to the vLLM server specified by the model_host and model_port parameters. Returns True if
         the connection test succeeds, and False if it fails.
         '''
-        logger.debug(f"Attempting to connect to model server at {self.model_host}:{self.model_port}...")
+        logger.debug(
+            f"Attempting to connect to model server at {self.model_host}:{self.model_port}...")
 
         if not vllm_health(self.model_host, self.model_port):
             logger.error(f"Cannot reach vLLM server. Start a server on {self.model_host}:{self.model_port} or "
@@ -184,7 +200,8 @@ class MACGPi:
             logger.debug(f"Checking phase validity of phase \"{phase}\"")
 
             phase_config: dict = phases_config[phase]
-            phase_path: str = os.path.join(self.templateManager.prompt_dir, phase_config["path"])
+            phase_path: str = os.path.join(
+                self.templateManager.prompt_dir, phase_config["path"])
             schema_required: bool = phase_config["schema"]
 
             if (not is_phase_dir(phase_path, schema_required=schema_required)):
@@ -218,7 +235,8 @@ class MACGPi:
         and present in the phases configuration, and that all phases contain valid phase directories. Returns True if
         the configuration is valid, and False otherwise.
         '''
-        schema_path: str = os.path.join(os.path.dirname(__file__), "..", "configs", "macgpi_config.schema.json")
+        schema_path: str = os.path.join(os.path.dirname(
+            __file__), "..", "configs", "macgpi_config.schema.json")
         with open(schema_path, "r") as f:
             schema: dict = json.load(f)
 
@@ -234,17 +252,20 @@ class MACGPi:
         if "entry" in macgpi_config.keys():
             entry_phase: str = macgpi_config["entry"]
             if entry_phase not in phases:
-                logger.error(f"Specified entry phase \"{entry_phase}\" not present in phase configuration!")
+                logger.error(
+                    f"Specified entry phase \"{entry_phase}\" not present in phase configuration!")
                 return False
 
         # Test if all phases contain valid next phases
         for phase_name, phase_config in macgpi_config["phases"].items():
             next_phase: str | None = phase_config.get("next", None)
             if next_phase is None or next_phase not in [*phases, "finish", "dynamic"]:
-                logger.error(f"Phase {phase_name} contains invalid next phase \"{next_phase}\"!")
+                logger.error(
+                    f"Phase {phase_name} contains invalid next phase \"{next_phase}\"!")
                 return False
 
-            max_visits_exceeded_next: str | None = phase_config.get("max_visits_exceeded_next", None)
+            max_visits_exceeded_next: str | None = phase_config.get(
+                "max_visits_exceeded_next", None)
             if max_visits_exceeded_next is not None and max_visits_exceeded_next not in [*phases, "finish"]:
                 logger.error(f"Phase {phase_name} contains invalid max_visits_exceeded_next phase "
                              + f"\"{max_visits_exceeded_next}\"!")
@@ -259,13 +280,16 @@ class MACGPi:
         '''
         logger.debug("Parsing phase configuration...")
 
-        macgpi_config: dict | None = parse_phase_config(self.phases_config_file)
+        macgpi_config: dict | None = parse_phase_config(
+            self.phases_config_file)
         if macgpi_config is None:
-            logger.error("Failed to parse phase configuration, cannot run MACGPi.")
+            logger.error(
+                "Failed to parse phase configuration, cannot run MACGPi.")
             raise Exception("Failed to parse phase configuration.")
 
         phases_config: dict = macgpi_config["phases"]
-        self.phase_visits: dict = {phase_name: 0 for phase_name in phases_config.keys()}
+        self.phase_visits: dict = {
+            phase_name: 0 for phase_name in phases_config.keys()}
 
         logger.debug("Phase configuration parsed successfully.")
 
@@ -280,7 +304,8 @@ class MACGPi:
         logger.debug("Executing pre-execution validation checks")
 
         if (self.templateManager is None or self.agentManager is None):
-            logger.error("MACGPi was not initialized successfully, cannot run pipeline.")
+            logger.error(
+                "MACGPi was not initialized successfully, cannot run pipeline.")
             return False
 
         # Test whether all phases contain valid phase directories
@@ -295,9 +320,11 @@ class MACGPi:
         Writes the project description to the output directory. This is useful for providing the project description as
         a reference for the prompts and agents during execution.
         '''
-        logger.debug(f"Writing project description to output directory at {self.output_dir}...")
+        logger.debug(
+            f"Writing project description to output directory at {self.output_dir}...")
 
-        project_description_path: str = os.path.join(self.output_dir, "docs", "project_description.md")
+        project_description_path: str = os.path.join(
+            self.output_dir, "docs", "project_description.md")
         os.makedirs(os.path.dirname(project_description_path), exist_ok=True)
 
         with open(project_description_path, "w") as f:
